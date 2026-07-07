a50572ed347f93249884feff0269e071a6da65e9
Ganesh-1424 <kaganesh04@gmail.com>
2026-07-07 22:38:24 +0530

    Time: 4 ms (38.41%) | Memory: 43.5 MB (26.20%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,50 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st=new Stack<>();
+        for(char ch:s.toCharArray()){
+            if(ch=='('||ch=='{'||ch=='['){
+               st.push(ch);
+            }
+            else{ 
+                if(st.isEmpty()){
+                    return false;
+                }
+                 char top = st.pop();
+                
+                if (ch == ')' && top != '(') return false;
+                if (ch == '}' && top != '{') return false;
+                if (ch == ']' && top != '[') return false;
+                 
+                // else {
+                //     return false;
+                // }
+            }
+        }
+   return st.isEmpty(); }
+//            Stack<Character>st=new Stack<>();
+//            for(char ch: s.toCharArray()){
+//              if(ch=='('||ch=='['||ch=='{'){
+//                st.push(ch);    
+//             }
+//             else{
+//                 if(st.isEmpty()){
+//                                         return true;
+//                                         }
+                
+//                 if(ch==')' && st.peek()!='(') return false;
+//                 if(ch==']' && st.peek()!='[') return false;
+//                 if(ch=='}' && st.peek()!='{') return false;
+                
+                
+//                 st.pop();
+//             }
+
+//            }
+//  return st.isEmpty();
+    // }
+}
+
+   
+
+
+    
